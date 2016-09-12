85805ade781fb499b10c3900254db459f6356d4c
wangxin <majort@qq.com>
2016-09-12 10:31:14 +0800

    Site updated: 2016-09-12 10:31:14

--- js/script.js
+++ js/script.js
@@ -135,3 +135,21 @@
     $container.removeClass('mobile-nav-on');
   });
 })(jQuery);
+
+(function(T,h,i,n,k,P,a,g,e){g=function(){P=h.createElement(i);a=h.getElementsByTagName(i)[0];P.src=k;P.charset="utf-8";P.async=1;a.parentNode.insertBefore(P,a)};T["ThinkPageWeatherWidgetObject"]=n;T[n]||(T[n]=function(){(T[n].q=T[n].q||[]).push(arguments)});T[n].l=+new Date();if(T.attachEvent){T.attachEvent("onload",g)}else{T.addEventListener("load",g,false)}}(window,document,"script","tpwidget","//widget.thinkpage.cn/widget/chameleon.js"))
+tpwidget("init", {
+    "flavor": "bubble",
+    "location": "WX4FBXXFKE4F",
+    "geolocation": "disabled",
+    "position": "top-left",
+    "margin": "10px 10px",
+    "language": "zh-chs",
+    "unit": "c",
+    "theme": "chameleon",
+    "uid": "U6663789AA",
+    "hash": "3101f7f7bd9ffd284f1e3a48be5f78a6"
+});
+tpwidget("show");
+
+
+
